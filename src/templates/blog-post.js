bb0388d08cf12a01120358877ad1e480666caf1b
Davide Scarioni <davide.scarioni@gmail.com>
2022-08-07 10:56:39 +0200

    Fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -29,7 +29,9 @@ const BlogPostTemplate = ({
           itemProp="articleBody"
         />
         <hr />
-        <Link to="/" className="link__back">🠐 Torna alla pagina precedente</Link>
+        <Link to="/" className="link__back"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+</svg> Torna alla pagina precedente</Link>
       </article>
     </Layout>
     </>
